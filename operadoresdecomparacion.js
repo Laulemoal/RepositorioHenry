64fb5e4c47f3f6cf98f9953209263425194d0327
Laulemoal <131291619+Laulemoal@users.noreply.github.com>
2023-04-20 14:20:55 -0300

    Add files via upload

---
+++ operadoresdecomparacion.js
@@ -0,0 +1,20 @@
+// console.log(4 < 7);
+// console.log(4 < 1);
+// console.log(4 > 4);
+// console.log(4 == 7);
+
+// console.log(3 == 3);
+// console.log(3 === 3);
+// console.log(3 == "3");
+// console.log(3 === "3");
+// console.log(7 == 7);
+// console.log(7 === "7");
+
+// var a = 2;
+// var b = 2;
+// var c = (a = b);
+ 
+//console.log(a) //2;
+//console.log(b) //2;
+//console.log(c) //2;
+
